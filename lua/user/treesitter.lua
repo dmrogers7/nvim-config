52e19f418b7589df2e484114c9482fecd285f808
David Rogers <david_m_rogers@homedepot.com>
2022-06-18 15:38:41 -0400

    Explicitly list supported languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -5,8 +5,7 @@ end
 
 configs.setup {
   -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- phpdoc fails on arm64, so this is entire list from https://github.com/nvim-treesitter/nvim-treesitter except phpdoc
-  ensure_installed = "astro bash beancount bibtex c c_sharp clojure cmake comment commonlisp cooklang cpp css cuda d dart devicetree dockerfile dot eex elixir elm elvish embedded_template erlang fennel fish foam fortran fusion Godot gleam Glimmer glsl go Godot gomod gowork graphql hack haskell hcl heex help hjson hocon html http java javascript jsdoc json json5 JSON julia kotlin lalrpop latex ledger llvm lua m68k make markdown ninja nix norg ocaml ocaml_interface ocamllex org pascal perl php pioasm prisma proto pug python ql Tree-sitter r rasi regex rego rst ruby rust scala scheme scss slint solidity sparql supercollider surface svelte swift teal tlaplus todotxt toml tsx turtle typescript v vala verilog vim vue wgsl yaml yang zig",
+  ensure_installed = "bash clojure cmake comment cpp css dart dockerfile dot go gomod gowork graphql hcl help hjson html http java javascript jsdoc json json5 JSON kotlin lua make markdown proto pug python Tree-sitter regex rego rust scala scheme scss slint todotxt toml tsx typescript vim vue yaml",
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
